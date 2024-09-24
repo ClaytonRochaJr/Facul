d32238d2c156bdfd5885f2ee803cf3439049446a
ClaytonRochaJr <claytonrocha179@gmail.com>
2024-09-24 19:37:19 -0300

    adicionando comandos em test

--- Testes/test.js
+++ Testes/test.js
@@ -2,3 +2,8 @@ const contas = ["agua", "luz", "gas", "comida", "energia"]
 const contasPagas = contas.splice(1,2)
 console.log("Antes: "+contas);
 console.log("Deois: "+contas);
+
+const bla = ["a", "b", "c", "d", "e"]
+const bla2 = bla.splice(1,2)
+console.log("Antes: "+bla);
+
